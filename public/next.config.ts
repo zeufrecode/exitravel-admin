1e0f2b4b31da95cc3d667b555fed2ef8ce7758ad
David Judrel Gnondabeka <judrelgnondabeka@gmail.com>
2025-11-19 07:31:01 +0100

    mise en ligne

--- public/next.config.ts
+++ public/next.config.ts
@@ -1,17 +1,22 @@
+// next.config.ts
 import withPWA from "next-pwa";
 import type { NextConfig } from "next";
 
+// Configuration principale Next.js
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  turbopack: {}, // neutralise Turbopack si nécessaire
+  turbopack: {}, // neutre, évite l'erreur Turbopack
 };
 
-// options PWA
+// Configuration PWA
 const pwaConfig = {
   dest: "public",
+  register: true,
+  skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
 };
 
+// Export final
 export default withPWA({
   ...nextConfig,
   ...pwaConfig,
